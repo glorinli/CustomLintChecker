d77e8e1e0e2683aeef99cf208e27c061b09fc378
glorinli <ligr1991@gmail.com>
2020-10-30 22:47:33 +0800

    Initial commit

---
+++ checker/src/main/java/xyz/glorin/customlint/checker/AndroidLogDetector.kt
@@ -0,0 +1,7 @@
+package xyz.glorin.customlint.checker
+
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.SourceCodeScanner
+
+class AndroidLogDetector : Detector(), SourceCodeScanner {
+}
---
+++ checker/src/main/java/xyz/glorin/customlint/checker/CustomRegistry.kt
@@ -0,0 +1,4 @@
+package xyz.glorin.customlint.checker
+
+class CustomRegistry {
+}
